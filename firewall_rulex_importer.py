8e64b51b3e39b37c4250abd57be5b9639865a8e0
Roman Silinenko <r.silinenko@gmail.com>
2020-10-28 11:55:18 +0300

    Base Firewall export\import tool

---
+++ firewall_rulex_importer.py
@@ -0,0 +1,339 @@
+import xmlrpclib
+import json
+import argparse
+import itertools
+import sys
+import os
+import time
+
+
+# Are we ready to import?
+files = ['zones.json','firewall_rules.json','network_objects.json','services.json']
+for fileName in files:
+    if not os.path.isfile(fileName):
+        sys.exit('Warning! Can\'t find '+fileName+' file. Run rules export script first. Exiting...')
+        
+
+#Just a spinning dash for terminal output
+spinner = itertools.cycle(['-', '/', '|', '\\'])
+
+
+def foundZoneByName(zonesList, zoneName):
+    for zone in zonesList:
+        if zone['name'] == zoneName:
+            return True
+    return False
+
+def findL7AppIDByName(l7Catalog, l7AppName):
+    for app in l7Catalog:
+        if app['name'] == str(l7AppName):
+            return cat['app_id']       
+
+def findL7CatIDByName(l7Catalog, l7CatName):
+    for cat in l7Catalog:
+        if cat['name'] == str(l7CatName):
+            return cat['id']   
+
+def findZoneIDByName(zonesList, zoneName):
+    for zone in zonesList:
+        if zone['name'] == str(zoneName):
+            return zone['id']
+
+def findServiceIDByName(seerviceList, serviceName):
+    for service in seerviceList:
+        if service['name'] == str(serviceName):
+            return service['id']  
+
+def findNetIDByName(networksList, networkName):
+    for net in networksList:
+        if net['name'] == str(networkName):
+            return net['id']
+
+
+argparser = argparse.ArgumentParser(description='Process command line params')
+argparser.add_argument("-s", "--server", required=True, help="Provide usergate appliance IP address")
+argparser.add_argument("-u", "--user", required=False, default='Admin', help="Admin login name (default is Admin)")
+argparser.add_argument("-p", "--passwd", required=False, default='', help="User password (default is blank)")
+args = argparser.parse_args()
+
+#Connect to API endpoint
+try:
+	server = xmlrpclib.ServerProxy("http://%s:4040/rpc" % args.server, allow_none=1)
+except Exception as err:
+    sys.exit(err)
+
+
+print("Successfully connected to " + server.v2.core.node.status()['node_name'])
+
+
+#Authenticate and get AUTH TOKEN
+try:
+    token = server.v2.core.login(args.user,args.passwd,{})["auth_token"]
+except Exception as err:
+    sys.exit(err)
+
+
+print("Ok, we are in, the token is " + token)
+
+
+##############################################################
+#                -=[ Delete old FW Rules ]=-                 #
+# NOTE: we have to delete 'em BEFORE objects in these rules. #
+##############################################################
+
+#       Pull firewall rules from UTM we are importing in.
+##############################################################
+try:
+    fwRules = server.v1.firewall.rules.list(token,0,1000,{})
+except Exception as err:
+    sys.exit(err)
+
+#       Remove all defined firewall rules. 
+##############################################################
+print '*\tNow clearing firewall rules...'
+
+for fwRule in fwRules['items']:
+    sys.stdout.write(next(spinner))
+    sys.stdout.flush()
+    sys.stdout.write('\b')
+    time.sleep(0.1)
+
+    try:
+        server.v1.firewall.rule.delete(token, fwRule['id'])
+    except Exception as err:
+        print err
+    print '\trule ', fwRule['name'], ' removed.' 
+    
+
+
+############################################################
+#                -=[ Import Zones ]=-                      #
+############################################################
+
+#       Load zone objects from file
+#################################################
+print '*\tImporting Zones...'
+try:
+    with open('zones.json', 'r') as fh:
+        importZones = json.load(fh)  
+except Exception as err:
+    sys.exit(err)
+print 'file zones.json loaded.'
+print 'found ',len(importZones),' zones.'
+
+#       Pull zones from UTM
+#################################################
+try:
+    UTMzones = server.v1.netmanager.zones.list(token)
+except Exception as err:
+    sys.exit(err)
+
+#Since we cannot just remove existing zones, we will created new zones based on exported data. 
+#The user will have to fine tune this crap by hands after import.
+c = 0
+for zone in importZones:
+    #We will import only zones we did not find in UTM we importing in. e.g. if we see there is already 'Trusted' zone, we will not import it.
+    if not foundZoneByName(UTMzones, zone['name']):
+        try:
+            print 'Importing zone: '+zone['name']
+            server.v1.netmanager.zone.add(token, zone)
+            c +=1
+        except Exception as err:
+            print err
+    
+print '*\tImported ',c, ' Zones'
+c = 0
+
+############################################################
+#   -=[ Import user defined network objects ]=-           #
+############################################################
+
+#       Load network objects info from file
+#################################################
+print '*\tImporting Network Objects...'
+try:
+    with open('network_objects.json', 'r') as fh:
+        ImportedNetworkObjects = json.load(fh)  
+except Exception as err:
+    sys.exit(err)
+fh.close()
+print 'file network_objects.json loaded.'
+print 'found ',len(ImportedNetworkObjects),' network objects.'
+
+#       Pull network lists from UTM we are importing in.
+##############################################################
+try:
+    UTM2NetworksList = server.v2.nlists.list(token, 'network',0,1000,'')['items']
+except Exception as err:
+    print err
+
+#       Remove all lists we can remove (pass built in lists)
+##############################################################
+print 'clearing network objects...'
+for ListItem in UTM2NetworksList:
+    if ListItem['editable']:
+        try:
+            server.v2.nlists.delete(token, ListItem['id'])
+        except Exception as err:
+            print err
+
+#       Import list of network objects
+##############################################################
+for ListItem in ImportedNetworkObjects:
+    netObjContents = ListItem.pop('netObjContents')
+    try:
+        print 'Importing IP-list: '+ListItem['name']
+        uid = server.v2.nlists.add(token, ListItem)
+        server.v2.nlists.list.add.items(token, uid, netObjContents)
+        c += 1
+    except Exception as err:
+        print err   
+print '*\tImported ',c, ' network objects'
+c = 0
+
+############################################################
+#   -=[ Import user defined services objects ]=-           #
+############################################################
+print '*\tImporting User defined Services Objects...'
+#       Load services info from file
+#################################################
+try:
+    with open('services.json', 'r') as fh:
+        ImportedServicesObjects = json.load(fh)  
+except Exception as err:
+    sys.exit(err)
+print 'file services.json loaded.'
+print 'found ',len(ImportedServicesObjects),' services objects.'
+
+#       Pull network lists from UTM we are importing in.
+##############################################################
+try:
+    UTM2ServicesList = server.v1.libraries.services.fetch(token,range(100,9999))
+except Exception as err:
+    print err
+
+#       Remove all defined services. 
+##############################################################
+print 'clearing service definitions...'
+if len(UTM2ServicesList) > 0:
+    for service in UTM2ServicesList:
+        try:
+            server.v1.libraries.service.delete(token,service['id'])
+        except Exception as err:
+            print err          
+
+#       Import list of services objects
+##############################################################
+for ListItem in ImportedServicesObjects:
+    try:
+        print 'Importing Service definition: '+ListItem['name']
+        server.v1.libraries.service.add(token, ListItem)
+        c +=1
+    except Exception as err:
+        print err   
+print '*\tImported ',c, ' service objects'
+c = 0
+
+############################################################
+#           -=[ Import firewall rules ]=-                  #
+############################################################
+
+
+#       Load firewall rules info from file
+#################################################
+try:
+    with open('firewall_rules.json', 'r') as fh:
+        importFirewallrules = json.load(fh)  
+except Exception as err:
+    sys.exit(err)
+fh.close()
+print 'firewall_rules.json loaded.'
+print 'found '+str(importFirewallrules['count'])+' rules.'
+rules_left = importFirewallrules['count']
+
+
+#       Import firewall rules
+##############################################################
+try:
+    #Pull updated zones list from UTM
+    UTM2zones = server.v1.netmanager.zones.list(token)
+    #Pull updated services list from UTM
+    UTM2ServicesList = server.v1.libraries.services.fetch(token,range(100,9999))
+    #print 'Got updated service list. Total '+ str(len(UTM2ServicesList))+' service definitions'
+    #Pull updated networks list from UTM
+    UTM2NetworksList = server.v2.nlists.list(token, 'network',0,1000,'')['items']
+    #Pull L7 apps catalog from UTM
+    #UTM2L7AppCatalog = server.v2.nlists.list(token, 'network',0,1000,'')['items']
+except Exception as err:
+    print err
+
+print '****  -=[ Starting Firewall rules IMPORT ]=-   ****'
+for importFwRule in importFirewallrules['items']:
+    #replace SRC zone names with local IDs
+    for index, SrcZone  in enumerate(importFwRule['src_zones']):
+        importFwRule['src_zones'][index] = findZoneIDByName(UTMzones, SrcZone)
+    #replace DST zone names with local IDs
+    for index, DstZone in enumerate(importFwRule['dst_zones']):
+        importFwRule['dst_zones'][index] = findZoneIDByName(UTMzones, DstZone)
+    
+    #replace SRC IP list names with local IDs
+    for index, SrcIP in enumerate(importFwRule['src_ips']):
+        listId, SrcIP_name = SrcIP
+        importFwRule['src_ips'][index] = [listId, findNetIDByName(UTM2NetworksList, SrcIP_name)]
+    #replace DST IP list names with local IDs
+    for index, DstIP in enumerate(importFwRule['dst_ips']):
+        listId, DstIP_name = DstIP
+        importFwRule['dst_ips'][index] = [listId, findNetIDByName(UTM2NetworksList, DstIP_name)]
+
+    #replace service names with lical IDs
+    for index, ServiceName in enumerate(importFwRule['services']):
+        importFwRule['services'][index] = findServiceIDByName(UTM2ServicesList, ServiceName)
+        
+    
+    importFwRule['apps'] = []
+    #replace L7 Application names with local IDs
+#    newAppsList = list()
+#    if len(importFwRule['apps']) > 0:
+#        #if it is not empty, then last element is the readable list of applications. we pasted it during export process.
+#        for app in importFwRule['apps'].pop():
+#            if app[0] == 'app':
+#                newAppsList.append([app[0],findL7AppIDByName()])          
+#            elif app[0] == 'ro_group':
+#                newAppsList.append([app[0],findL7CatIDByName()])
+#            else:
+#                pass
+#        importFwRule['apps'] = newAppsList
+#    del newAppsList
+    
+    
+    print 'Importing rule:',' [',importFirewallrules['count'],'/',importFirewallrules['count'] - rules_left+1,']', importFwRule['name']
+    #print 'Service: '+str(importFwRule['services'])
+    #time.sleep(10)
+    try:
+        server.v1.firewall.rule.add(token, importFwRule)
+        rules_left -=1
+    except Exception as err:
+        print err
+        print '!!! Not imported:',' [',importFirewallrules['count'],'/',importFirewallrules['count'] - rules_left+1,']', importFwRule['name']
+        c+=1
+        pass
+    c+=1
+
+print '*\tImported ',c, ' Firewall rules'
+c = 0
+
+
+#Orignal
+#In [56]: fwRules['items'][7]['services']
+#Out[56]: [120, 121, 158, 104]
+
+#In [57]: fwRules['items'][7]['apps']
+#Out[57]: [['ro_group', 4], ['ro_group', 6], ['app', 7738], ['app', 230], ['app', 5577]]
+
+#In [58]: fwRules['items'][7]['dst_zones']
+#Out[58]: [2, 4, 1]
+
+#In [59]: fwRules['items'][7]['dst_ips']
+#Out[59]: [['list_id', 1039], ['list_id', 1002], ['list_id', 1018]]
+
+
